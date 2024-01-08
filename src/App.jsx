5ba24efef68d73b87d6b6454c0bcb1ecad523309
Tasin1025 <tasintt@gmail.com>
2024-01-08 23:37:50 +0600

    form

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,18 @@ function App() {
 
   return (
     <>
-      
+      <nav >
+        <ul className=' bg-red-600 flex justify-center gap-5'>
+          <li className='px-5 py-3 m-5 text-2xl font-bold font-mono border-solid border-stone-900 border-4 '> Home </li>
+          <li className='px-5 py-3 m-5 text-2xl font-bold font-mono border-solid border-stone-900 border-4 '> registration </li>
+        </ul>
+      </nav>
+      {/* form */}
+      <div className="flex gap-5">
+        <input type="text" className='border border-4 border-solid border-stone-900' />
+        <input type="text" className='border border-4 border-solid border-stone-900' />
+        <input type="button" value="Login" className='px-5 border border-solid border-stone-900 border-4'  />
+      </div>
 
     </>
   )
